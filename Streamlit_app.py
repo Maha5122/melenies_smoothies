9e98b36ccf0822f7ac03d1512dea2827f8694d16
Mahaalakshmi <103752949+Maha5122@users.noreply.github.com>
2024-07-27 16:48:36 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -49,5 +49,5 @@ if ingredients_list:
 # new section to display neutrion infromation
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
 fv.df = st.dataframe(data = fruityvice_response.json(), use_container_width =True)
