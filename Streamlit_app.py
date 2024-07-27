587743a2a061cdd58f3f7ad37259f943433ab8ad
Mahaalakshmi <103752949+Maha5122@users.noreply.github.com>
2024-07-27 17:15:36 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-C
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Somoothie! :cup_with_straw:")
